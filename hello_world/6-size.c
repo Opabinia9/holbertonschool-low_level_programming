4e3a8891a7cf7ce393d66aefa5918e5476bb0ffd
Sebastion <sebastionprice@gmail.com>
2026-02-10 01:16:13 +0000

    added 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* this is a printing program */
+
+/**
+ * main - this is the main function
+ * Return: always return 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
+	return (0);
+}
