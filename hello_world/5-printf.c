48954d3ae04c592429e4fac2d669e7376cebbd30
Sebastion <sebastionprice@gmail.com>
2026-02-10 00:46:43 +0000

    added 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* this is a printing program */
+
+/**
+ * main - this is the main function
+ * Return: always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
