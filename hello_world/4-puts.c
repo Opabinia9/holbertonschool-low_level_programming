8f2af51ef61c831f800ea2659a1425f50af6e921
Sebastion <sebastionprice@gmail.com>
2026-02-10 00:42:17 +0000

    apeased betty for 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/* this is a printing program */
 
-
+/**
+ * main - this is the main function
+ * Return: always return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
