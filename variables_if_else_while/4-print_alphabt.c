b4ba20d88fdd8c9ba125f37cd5249e1bcff85a75
Sebastion <sebastionprice@gmail.com>
2026-02-11 00:29:08 +0000

    added 4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
