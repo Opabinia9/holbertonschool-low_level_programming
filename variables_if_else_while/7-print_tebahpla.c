ebdd4189c6091af695a601bf7e66795ea58931df
Sebastion <sebastionprice@gmail.com>
2026-02-11 04:06:45 +0000

    added 7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
