314bf26888a1c51845b0b2f12f2f122e9ba0d14d
Sebastion <sebastionprice@gmail.com>
2026-02-11 04:38:26 +0000

    addded 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+		if (a != 58)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
