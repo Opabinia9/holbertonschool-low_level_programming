6cf4bea8dae12fe72e038cc35fec8f28c95156a2
Sebastion <sebastionprice@gmail.com>
2026-02-14 04:03:21 +0000

    added 9-times_table.c and deps

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+void print_dd(int n)
+{
+	if (n < 10)
+	{
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else 
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
+/**
+ */
+void times_table(void)
+{
+	int c, n, r;
+	c = 0;
+
+	while (c < 10)
+	{
+		n = r = 0;
+		while (n < 10)
+		{
+			if (n == 0)
+			{
+				_putchar(r + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				print_dd(r);
+			}
+			r+=c;
+			n++;
+		}
+		_putchar('\n');
+		c++;
+	}
+}
