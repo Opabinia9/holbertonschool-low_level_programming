56758e7bbb773c936e9ab10c181d90a696f156a2
Sebastion <sebastionprice@gmail.com>
2026-02-16 01:46:13 +0000

    added 6-print_line.c and deps

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - function to print a vertical line
+ * @n: the width of the line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
