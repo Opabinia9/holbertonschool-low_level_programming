ef2a040a4283f49e940071715ad0ba0baf23cd86
Sebastion <sebastionprice@gmail.com>
2026-02-16 02:50:55 +0000

    added 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main function, (hopefully) passes the fizzbuzz test
+ * Return - main always returns 0 on succsess
+ */
+int main (void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
