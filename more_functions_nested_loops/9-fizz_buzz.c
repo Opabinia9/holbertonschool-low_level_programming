2450b6e768f15c30bbd23bf03d894ee2a4df723d
Sebastion <sebastionprice@gmail.com>
2026-02-16 02:53:24 +0000

    fixed end space

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,25 @@ int main (void)
 
 	while (n <= 100)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (n == 1)
 		{
-			printf("FizzBuzz ");
+			printf("%d", n);
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf(" %d", n);
 		}
 		n++;
 	}
