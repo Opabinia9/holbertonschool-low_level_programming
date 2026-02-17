89038c5e0a401be8d0a740861bcd1d3ab3a190cf
Sebastion <sebastionprice@gmail.com>
2026-02-17 12:04:38 +0000

    added 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a given string
+ * @s: string to check
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+
+/**
+ * print_rev - prints a string in revers, with newline
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int a;
+
+	a = _strlen(s);
+	do
+	{
+		a--;
+		_putchar(s[a]);
+	} while (a != 0);
+	_putchar('\n');
+}
