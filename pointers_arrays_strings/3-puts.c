0e16ec417485a0ec60695e2a7230c02d69d9ef6f
Sebastion <sebastionprice@gmail.com>
2026-02-17 11:49:21 +0000

    added 3-puts.c and deps

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a newline
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
