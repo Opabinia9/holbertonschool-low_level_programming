d9b1e7ddb770c0aa3f188ac4656ca22d9db141c9
Sebastionprice <sebastionprice@gmail.com>
2026-02-21 11:19:29 +0000

    added 1-strncat.c and deps

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a given string
+ * @s: string to check
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+
+/**
+ * _strncat - concatinate src to dest using max of n from src
+ * @dest: string to add to
+ * @src: string to add to dest
+ * Return: the pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = _strlen(dest);
+	int y = 0;
+
+	while (src[y] && y < n)
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	return (dest);
+}
