dfe20511b1486fa369e0e72ebf2250613c7fd86a
Sebastionprice <sebastionprice@gmail.com>
2026-02-23 02:18:16 +0000

    added 1-memcpy.c and deps

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * char *_memcpy(char *dest, char *src, unsigned int n); -
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x = 0;
+
+	while (x < n)
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	return (dest);
+}
