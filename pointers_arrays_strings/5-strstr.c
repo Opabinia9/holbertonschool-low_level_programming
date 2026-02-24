81ce0a2b927c75e97381aacb12f6fa759950c52e
Sebastion <sebastionprice@gmail.com>
2026-02-24 23:48:51 +0000

    added 5-strstr.c and deps

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strstr - find the first ocurence of neeed;e in haystack
+ * @haystack: string to search
+ * @needle: string to find
+ * Return: pointer to first match of needle in haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x = 0;
+	int y;
+	int match;
+
+	while (haystack[x])
+	{
+		if (haystack[x] == needle[0])
+		{
+			y = 0;
+			match = 1;
+			while (needle[y])
+			{
+				if (!(haystack[x + y] == needle[y]))
+				{
+					match = 0;
+				}
+				y++;
+			}
+			if (match)
+			{
+				return (&haystack[x]);
+			}
+		}
+		x++;
+	}
+
+	return ('\0');
+}
