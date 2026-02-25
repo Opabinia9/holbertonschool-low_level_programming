1bb7ac7814fec286d6e5932ea21f01358fa1fc88
Sebastion <sebastionprice@gmail.com>
2026-02-25 00:51:31 +0000

    added 7-print_chessboard.c and deps

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard -
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x = 0;
+	int y;
+
+	while (x < 8)
+	{
+		y = 0;
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
