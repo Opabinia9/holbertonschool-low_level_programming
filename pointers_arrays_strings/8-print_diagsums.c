5ca7275ff5a5ea01f8428ab12bc69ff7d25eb1f4
Sebastion <sebastionprice@gmail.com>
2026-02-25 02:56:06 +0000

    fix math for diags

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,19 +9,14 @@
 void print_diagsums(int *a, int size)
 {
 	int i = 0;
-	int x = 0;
-	int n = size * size - size;
-	int y = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
-	while (i < (size * size))
+	while (i < (size))
 	{
-		x += a[i];
-		i += 1 + size;
+		sum1 += a[(i * size) + i];
+		sum2 += a[i * size + (size - 1 - i)];
+		i++;
 	}
-	while (n >= 0)
-	{
-		y += a[n];
-		n -= size - 1;
-	}
-	printf("%d, %d\n", x, y);
+	printf("%d, %d\n", sum1, sum2);
 }
