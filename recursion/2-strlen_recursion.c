91ce32cbe02a6e4aad5f57c48197d1cbd5fb01f7
Sebastionprice <sebastionprice@gmail.com>
2026-02-28 05:12:46 +0000

    added 2-strlen_recursion.c and deps

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to check
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+}
