6fade4aa6021a406402faf71e4630f2d13f1fcd6
Sebastionprice <sebastionprice@gmail.com>
2026-03-01 03:45:46 +0000

    added 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers from command line arguments
+ * @argc: number of command line argumants
+ * @argv: array of command line argumants
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc < 3)
+	{
+		return (1);
+	}
+	i = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", i);
+	return (0);
+}
