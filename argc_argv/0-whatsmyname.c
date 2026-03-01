92859614ab517a7add6bcfdf4c68c7fb4cae9926
Sebastionprice <sebastionprice@gmail.com>
2026-03-01 03:15:52 +0000

    added 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the binaray
+ * @argc: number of command line agruments passed
+ * @argv: array of command line arguments passed
+ * Return: 0 on succsess
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	return (0);
+}
