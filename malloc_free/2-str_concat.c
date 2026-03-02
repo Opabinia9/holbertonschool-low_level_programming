f3dfdb9e49cb7d019d20cb10a64a0fde05272f0a
Sebastionprice <sebastionprice@gmail.com>
2026-03-02 02:43:54 +0000

    added 2-str_concat.c and deps

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatanate two string
+ * @s1: first string
+ * @s2: second string
+ * Return: concatanated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int n = 0;
+	int i = 0;
+	int x = 0;
+	char *arr;
+
+	while (s1[n])
+	{
+		n++;
+	}
+	while (s2[i])
+	{
+		i++;
+	}
+	i++;
+	arr = (char *)malloc((n + i) * sizeof(*arr));
+	while (*s1)
+	{
+		arr[x] = *s1;
+		s1++;
+		x++;
+	}
+	while (*s2)
+	{
+		arr[x] = *s2;
+		s2++;
+		x++;
+	}
+	arr[x] = '\0';
+	return (arr);
+}
