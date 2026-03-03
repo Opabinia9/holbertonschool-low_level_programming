6f9b7444fac542cea531a03c220d53ce575acea6
Sebastionprice <sebastionprice@gmail.com>
2026-03-03 23:37:59 +0000

    added 2-calloc.c and deps

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size of memory to allocate
+ * Return: pointer to memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	char *arr2;
+	unsigned int x = 0;
+
+	if ((nmemb || size) == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (!arr)
+		return (NULL);
+	arr2 = arr;
+	while (x < nmemb)
+	{
+		arr2[x] = 0;
+		x++;
+	}
+	return (arr);
+}
