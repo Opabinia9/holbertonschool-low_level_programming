3f127b7f3c937b38c24c658b367a69c887b1eddf
Sebastionprice <sebastionprice@gmail.com>
2026-03-04 03:18:35 +0000

    add: dog.h and deps

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+
+/**
+ * struct dog - dog data type
+ * @age: age of dog
+ * @name: name of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	float age;
+	char *name;
+	char *owner;
+};
+
+#endif
