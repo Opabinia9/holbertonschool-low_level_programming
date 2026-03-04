71263dfafbea3c3c24510d1dd9bd13353859da2e
Sebastionprice <sebastionprice@gmail.com>
2026-03-04 03:38:44 +0000

    exit if variable null

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * init_dog - initalize a variable of dog type
  * @d: pointer to variable to initialize
@@ -9,6 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		exit(1);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
