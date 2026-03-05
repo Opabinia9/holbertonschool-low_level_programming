abb1e0805aaf739641663ed4de4605e8dd643fc4
Sebastionprice <sebastionprice@gmail.com>
2026-03-05 04:26:59 +0000

    fix 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,9 +15,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog1 = malloc(sizeof(dog_t));
 	if (!dog1)
 		return (NULL);
-	dog1->name = name;
+	dog1->name = malloc(sizeof(name));
+	if (!dog1->name)
+	{
+		free(dog1);
+		return (NULL);
+	}
+	dog1->owner = malloc(sizeof(owner));
+	if (!dog1->owner)
+	{
+		free(dog1->name);
+		free(dog1);
+		return (NULL);
+	}
+	(*dog1).name = name;
 	dog1->age = age;
-	dog1->owner = owner;
+	(*dog1).owner = owner;
 
 	return (dog1);
 }
