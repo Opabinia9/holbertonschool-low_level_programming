2971c8c8a44582f33895a01478fe3885e18caced
Sebastionprice <sebastionprice@gmail.com>
2026-03-05 04:18:52 +0000

    4-new_dog.c and deps

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create a variable of dog_t type
+ * @name: value of name field
+ * @age: value for age field
+ * @owner: value for owner field
+ * Return: pointer to new variable
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog1;
+
+	dog1 = malloc(sizeof(dog_t));
+	if (!dog1)
+		return (NULL);
+	dog1->name = name;
+	dog1->age = age;
+	dog1->owner = owner;
+
+	return (dog1);
+}
