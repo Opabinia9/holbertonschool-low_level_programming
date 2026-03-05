d4627a91c2be89620c68abf8f6ec48643c1b7c83
Sebastionprice <sebastionprice@gmail.com>
2026-03-05 04:05:43 +0000

    add 3-main.c and deps

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @name: name of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	float age;
 	char *name;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
