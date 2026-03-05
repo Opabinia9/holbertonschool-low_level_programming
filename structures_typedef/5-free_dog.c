3b9b3289ff8491fd6316f95be0843a261de12340
Sebastionprice <sebastionprice@gmail.com>
2026-03-05 10:44:34 +0000

    add 5-free_dog.c and deps

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free variables of type dog
+ * @d: variable to free
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
