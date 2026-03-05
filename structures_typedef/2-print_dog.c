505416022789704de579ad7d81c961a942c2c2c3
Sebastionprice <sebastionprice@gmail.com>
2026-03-05 04:00:27 +0000

    add 2-print_dog.c and deps

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - print a variable of the dog type
+ * @d: variable to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: %s\n", "nil");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: %s\n", "nil");
+	else
+		printf("Owner: %s\n", d->owner);
+}
