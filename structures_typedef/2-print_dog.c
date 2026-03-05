c6a9361b24e718bb5f4b6e2db859f7557884d935
Sebastionprice <sebastionprice@gmail.com>
2026-03-05 04:01:36 +0000

    renamed nill

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		printf("Name: %s\n", "nil");
+		printf("Name: %s\n", "(nil)");
 	else
 		printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Owner: %s\n", "nil");
+		printf("Owner: %s\n", "(nil)");
 	else
 		printf("Owner: %s\n", d->owner);
 }
