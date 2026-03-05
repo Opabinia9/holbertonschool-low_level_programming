a508d8e067d4c4b1f95f09c9d1f93ea6b5927108
Sebastionprice <sebastionprice@gmail.com>
2026-03-05 04:43:46 +0000

    fix 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,34 +16,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog1 = malloc(sizeof(dog_t));
 	if (!dog1)
 		return (NULL);
-	dog1->name = malloc(sizeof(name));
+	while (name[y])
+		y++;
+	dog1->name = malloc(y + 1);
 	if (!dog1->name)
 	{
 		free(dog1);
 		return (NULL);
 	}
-	dog1->owner = malloc(sizeof(owner));
+	while (x < y)
+	{
+		dog1->name[x] = name[x];
+		x++;
+	}
+	dog1->name[x] = '\0';
+	x = 0, y = 0;
+	while (owner[y])
+		y++;
+	dog1->owner = malloc(y + 1);
 	if (!dog1->owner)
 	{
 		free(dog1->name);
 		free(dog1);
 		return (NULL);
 	}
-	while (name[y])
-		y++;
-	while (x < y)
-	{
-		dog1->name[x] = name[x];
-		x++;
-	}
-	x = 0, y = 0;
-	while (owner[y])
-		y++;
 	while (x < y)
 	{
 		dog1->owner[x] = owner[x];
 		x++;
 	}
+	dog1->owner[x] = '\0';
 	dog1->age = age;
 
 	return (dog1);
