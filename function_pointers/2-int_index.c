00de9741d3cf8948f8c70f0ee404e749cc5046ef
Sebastionprice <sebastionprice@gmail.com>
2026-03-10 03:40:34 +0000

    Fix NULL case

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,6 +10,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int n = 0;
 
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	while (n < size)
