535ddbbe94c2a73201e553683e5884753a1be775
Sebastionprice <sebastionprice@gmail.com>
2026-03-10 03:37:36 +0000

    Add  2-int_index.c and deps

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+
+/**
+ * int_index - fint index of first match by cmp in array
+ * @array: array to check
+ * @size: size of array
+ * @cmp: checker
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int n = 0;
+
+	if (size <= 0)
+		return (-1);
+	while (n < size)
+	{
+		if (cmp(array[n]))
+			return (n);
+		n++;
+	}
+	return (-1);
+}
