6880500f1e7984e53b65a90488929ba3cf0cbcaf
Sebastionprice <sebastionprice@gmail.com>
2026-03-15 11:02:21 +0000

    Add 3-print_all.c and deps

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,67 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print int, float, char or string
+ * @format: type to print
+ */
+void print_all(const char * const format, ...)
+{
+	int i, n;
+	va_list varg;
+	op_t ops[] = {
+		{"c", cprint},
+		{"i", iprint},
+		{"f", fprint},
+		{"s", sprint},
+		{NULL, NULL}
+	};
+
+	va_start(varg, format);
+	i = 0;
+	while (format[i])
+	{
+		n = 0;
+		while (ops[n].op)
+		{
+			if (format[i] == ops[n].op[0])
+				ops[n].f(varg);
+			n++;
+		}
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * cprint - print char
+ * @vargc: char to print
+ */
+void cprint(va_list vargc)
+{
+	printf("%c", va_arg(vargc, int));
+}
+/**
+ * iprint - print int
+ * @vargi: int to print
+ */
+void iprint(va_list vargi)
+{
+	printf("%i", va_arg(vargi, int));
+}
+/**
+ * fprint - print float
+ * @vargf: float to print
+ */
+void fprint(va_list vargf)
+{
+	printf("%f", va_arg(vargf, double));
+}
+/**
+ * sprint - print string
+ * @vargs: string to print
+ */
+void sprint(va_list vargs)
+{
+	printf("%s", va_arg(vargs, char *));
+}
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,5 +8,21 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void cprint(va_list);
+void iprint(va_list);
+void fprint(va_list);
+void sprint(va_list);
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list);
+} op_t;
 
 #endif
