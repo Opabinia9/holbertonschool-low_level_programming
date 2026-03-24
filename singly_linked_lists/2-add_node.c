d68aa667f07bc2fb70f9d75582262d6781f33ded
Sebastion Price <sebastionprice@gmail.com>
2026-03-24 21:20:23 +1100

    Add 1-add_node.c and deps

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node - 
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i;
+	list_t *h = malloc(sizeof(list_t));
+
+	if (!h)
+		return (NULL);
+
+	for (i = 0; str[i]; )
+		i++;
+	printf("line 16, i: %d\n", i);
+	h->next = *head;
+	h->str = strdup(str);
+	h->len = i;
+	*head = h;
+	printf("line 20, str: %s\n", h->str);
+
+	return (h);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +24,6 @@ int _putchar(char c);
 size_t print_list(const list_t *h);
 char *itoa(int);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
