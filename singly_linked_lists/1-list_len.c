020d810b293cec89fa8cf118a07fc6d02fa19b6d
Sebastion Price <sebastionprice@gmail.com>
2026-03-24 14:25:42 +1100

    Apease betty for 1-list_len

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * list_len - return the length of a linked list
+ * @h: list to get the length of
+ * Return: the length of the list
+ */
 size_t list_len(const list_t *h)
 {
 	int n = 0;
