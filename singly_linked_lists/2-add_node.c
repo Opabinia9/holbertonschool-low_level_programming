36829d77fb93fb39b6dbd94d63d02c9e77973c2c
Sebastion Price <sebastionprice@gmail.com>
2026-03-24 21:32:05 +1100

    Apease betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 
 /**
- * add_node - 
+ * add_node - add node AND UPDATE HEAD
+ * @head: current head that will be UPDATED
+ * @str: string to give to new list_t node
+ * Return: address to new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
