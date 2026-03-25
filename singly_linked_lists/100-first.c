82797d0f1f6ba1732b1d9bb5baeb87609fd60e15
Sebastion Price <sebastionprice@gmail.com>
2026-03-25 19:21:53 +1100

    Add 100-first.c and deps

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * earlybird - print a message before main runs
+ */
+void earlybird(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
