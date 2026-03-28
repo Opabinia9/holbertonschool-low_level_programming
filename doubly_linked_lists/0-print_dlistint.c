3074af2983a7f7a5434514bd39de35270e13a71c
Sebastion Price <sebastionprice@gmail.com>
2026-03-28 23:13:36 +1100

    Apease betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 
 /**
+ * print_dlistint - print the value of each node from a linked list
+ * @h: pointer to head node of list
+ * Return: number of nodes in list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
