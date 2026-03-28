d4fbaa6dfd3abf68d58ac145b9b18d39a6216361
Sebastion Price <sebastionprice@gmail.com>
2026-03-28 23:51:26 +1100

    Fix head ref

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *node = malloc(sizeof(dlistint_t));
 
 	node->n = n;
-	if (head)
+	if (*head)
 		node->next = *head;
 	else
 		node->next = NULL;
