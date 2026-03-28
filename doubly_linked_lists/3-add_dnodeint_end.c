3b6616a0b8ad8b4f57e4641ed764ea22ee822385
Sebastion Price <sebastionprice@gmail.com>
2026-03-29 00:19:42 +1100

    Add 3-add_dnodeint_t.c and deps

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add new node to end of linked list
+ * @head: pointer to pointer to head of linked list
+ * @n: value for new node
+ * Return: pointer to new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *lhead = *head;
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+
+	if (!node)
+		return (NULL);
+	node->n = n;
+	node->next = NULL;
+	node->prev = *head;
+
+	if (lhead)
+	{
+		while (lhead->next)
+		{
+			lhead = lhead->next;
+		}
+		lhead->next = node;
+	}
+	else
+		*head = node;
+	return (node);
+}
