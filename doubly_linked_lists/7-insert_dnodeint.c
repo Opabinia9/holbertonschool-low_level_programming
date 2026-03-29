9ba28bc6adfb567a9ec3fca99beed77e0ad8acb1
Sebastion Price <sebastionprice@gmail.com>
2026-03-29 13:23:16 +1100

    Revert "Test remove end add suport"
    This reverts commit a5e71488915fe92228ee4376420e16d10e23591d.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (i < idx)
 	{
-		if (!head->next)
+		if (!head)
 			return (NULL);
 		prev = head;
 		head = head->next;
