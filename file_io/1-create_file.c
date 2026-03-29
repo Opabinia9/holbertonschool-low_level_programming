51d0bc8c6a0e66e364b0591bbb9146e99c3cca56
Sebastion Price <sebastionprice@gmail.com>
2026-03-29 17:22:30 +1100

    Fix used creat instead of open

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	for (len = 0; text_content[len];)
 		len++;
-	fd = creat(filename, 0600);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT);
 	if (fd == -1)
 		return (-1);
 	res = write(fd, text_content, len);
