1a0a005a6c7995dcde2178e76923bbff94387778
Sebastion Price <sebastionprice@gmail.com>
2026-03-29 23:10:39 +1100

    Test idx 0 handling v4

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,8 +27,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	node->n = n;
-	node->next = head;
-	node->prev = head->prev;
+	node->next = head->next;
+	node->prev = head;
 	if (node->prev)
 		node->prev->next = node;
 	if (node->next)
