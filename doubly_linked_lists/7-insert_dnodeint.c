e1c915da031d9e0f1d880ba3b72bbec031ff191a
Sebastion Price <sebastionprice@gmail.com>
2026-03-29 23:31:18 +1100

    Test idx 0 handling v11

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (i < (idx - 1) head != NULL)
+	while (i < (idx - 1) && head != NULL)
 	{
 		head = head->next;
 		i++;
