4c8d14f141c2f2299c7e3772e7e6851325ebe5b0
Sebastion Price <sebastionprice@gmail.com>
2026-03-30 00:00:26 +1100

    revert 7 to my version

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,39 +1,39 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index- inserts a node at an index number
- * @h: a doubly linked list
- * @idx: index to insert the node at
- * @n: integer value for the new node
- * Return: returns the address of the new node or NULL on failure
+ * insert_dnodeint_at_index - insert node at idx
+ * @h: current head
+ * @idx: index for new node
+ * @n: data for new node
+ * Return: pointer to new node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t)), *current;
 	unsigned int i = 0;
-
-	if (new == NULL)
-		return (NULL);
+	dlistint_t *node;
+	dlistint_t *head = *h;
+	dlistint_t *prev = NULL;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
-	current = *h;
-	while (current != NULL && i < idx - 1)
+	while (i < idx)
 	{
-		current = current->next;
+		if (!head)
+			return (NULL);
+		prev = head;
+		head = head->next;
 		i++;
 	}
-
-	if (current == NULL)
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
 		return (NULL);
 
-	new->n = n;
-	new->next = current->next;
-	new->prev = current;
-	if (current->next != NULL)
-		current->next->prev = new;
-	current->next = new;
-	return (new);
+	node->n = n;
+	node->next = head;
+	node->prev = prev;
+	if (node->prev)
+		node->prev->next = node;
+	if (node->next)
+		node->next->prev = node;
+	return (node);
 }
