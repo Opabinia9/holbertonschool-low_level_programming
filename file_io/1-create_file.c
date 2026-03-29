7e87e68dc8177210bcdd51b6798d3a7ca0092889
Sebastion Price <sebastionprice@gmail.com>
2026-03-29 17:30:03 +1100

    Fix return value

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,5 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	res = write(fd, text_content, len);
-	return (res);
+	if (res == -1)
+		return (-1);
+	return (1);
 }
