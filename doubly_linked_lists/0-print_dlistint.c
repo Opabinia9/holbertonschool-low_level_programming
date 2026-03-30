e7181ff4ca394981a45da2b2a0ef277c6539d0c5
Sebastion Price <sebastionprice@gmail.com>
2026-03-30 22:47:23 +1100

    Rework original to use printf

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,19 @@
 #include "lists.h"
 
 /**
- * print_dlistint- prints all the elements of a doubly linked list
- * @h: a doubly linked list
- * Return: the number of nodes.
+ * print_dlistint - print the value of each node from a linked list
+ * @h: pointer to head node of list
+ * Return: number of nodes in list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *current = h;
-	size_t count = 0;
+	int count = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (current != NULL)
+	while (h)
 	{
-		printf("%d\n", current->n);
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
-		current = current->next;
 	}
 	return (count);
 }
