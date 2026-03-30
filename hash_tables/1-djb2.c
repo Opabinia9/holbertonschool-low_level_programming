dac41c452be56420b3d3d072dfee8e835e451d88
Sebastion Price <sebastionprice@gmail.com>
2026-03-30 12:49:43 +1100

    Appease betty for 1-djb2.c

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,12 +1,17 @@
+/**
+ * hash_djb2 - hash a string with the sjb2 algorithm
+ * @str: string to hash
+ * Return: hash of str
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
