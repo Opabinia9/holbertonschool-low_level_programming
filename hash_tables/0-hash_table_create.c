54acca3581322ee283eed09f5669850a9785f5cb
Sebastion Price <sebastionprice@gmail.com>
2026-03-30 12:18:27 +1100

    Appease betty for 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,8 +1,14 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create a hash table
+ * @size: size of hash table array
+ * Return: pointer to hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hasht = malloc(sizeof(hash_table_t));
+
 	if (hasht == NULL)
 		return (NULL);
 
