583e2803d88017ca272ec8526e8ce6f1c4dd16fb
Sebastion Price <sebastionprice@gmail.com>
2026-03-31 11:15:23 +1100

    Add 2-key_index.c and deps

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * key_index - generate index for hash_table for a key
+ * key: the key
+ * size: the size of the array of the hash table
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int idx = hash_djb2(key);
+
+	if (!size)
+		return (0);
+
+	idx %= size;
+
+	return (idx);
+}
