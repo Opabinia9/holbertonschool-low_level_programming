f9342892b64a45a8cb51397a60cc039e8fb037d9
Sebastion Price <sebastionprice@gmail.com>
2026-03-31 11:17:38 +1100

    Update description for key_index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
  * key_index - generate index for hash_table for a key
- * key: the key
- * size: the size of the array of the hash table
+ * @key: the key
+ * @size: the size of the array of the hash table
  * Return: index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
